0d527512236d85ccf4ebca76e4212d12eb30aae1
haiibarose <ruk.rossukhon@gmail.com>
2023-04-27 19:58:06 +0700

    v.1.5

---
+++ controller/itemsController.js
@@ -0,0 +1,84 @@
+const dotenv = require("dotenv");
+dotenv.config();
+const { v4: uuidv4 } = require("uuid");
+const { DynamoDBClient } = require("@aws-sdk/client-dynamodb");
+const {
+  PutCommand,
+  DeleteCommand,
+  ScanCommand,
+} = require("@aws-sdk/lib-dynamodb");
+
+const docClient = new DynamoDBClient({ regions: process.env.AWS_REGION });
+
+exports.getGroupMembers = async (req, res) => {
+  const params = {
+    TableName: process.env.aws_group_members_table_name,
+  };
+  try {
+    const data = await docClient.send(new ScanCommand(params));
+    res.send(data.Items);
+  } catch (err) {
+    console.error(err);
+    res.status(500).send(err);
+  }
+};
+
+// TODO #1.1: Get items from DynamoDB
+exports.getItems = async (req, res) => {
+  // You should change the response below.
+  const params = {
+    TableName: process.env.aws_student_progress_table_name,
+  };
+
+  try {
+    const data = await docClient.send(new ScanCommand(params));
+    res.send(data.Items);
+  } catch (err) {
+    console.error(err);
+    res.status(500).send(err);
+  }
+};
+
+// TODO #1.2: Add an item to DynamoDB
+exports.addItem = async (req, res) => {
+  //const item_id = uuidv4();
+  //const created_date = Date.now();
+  // const student_id = req.params.student_id;
+  // const prog = req.params.prog;
+  const item = {...req.body};
+  const params = {
+    TableName: process.env.aws_student_progress_table_name,
+    Item: item,
+  };
+  console.log(params)
+
+  // You should change the response below.
+  try {
+    await docClient.send(new PutCommand(params));  
+    res.status(200).end();
+  } catch (err) {
+    console.error(err);
+    res.status(500).send(err);
+  }
+};
+
+// TODO #1.3: Delete an item from DynamDB
+exports.deleteItem = async (req, res) => {
+  const student_id = req.params.student_id;
+  const params = {
+    TableName: process.env.aws_student_progress_table_name,
+    Key: {
+      student_id: student_id
+    }
+  };
+  // You should change the response below.
+  try {
+    await docClient.send(new DeleteCommand(params));
+    res.status(200).end();
+  } catch (err) {
+    console.error(err);
+    res.status(500).send(err);
+  }
+};
+
+
---
+++ script_item.js
@@ -0,0 +1,138 @@
+// TODO #4.0: Change this IP address to EC2 instance public IP address when you are going to deploy this web application
+const backendIPAddress = "127.0.0.1:3000";
+
+let itemsData;
+
+// TODO #2.1: Edit group number
+const getGroupNumber = () => {
+  return 14;
+};
+
+// TODO #2.2: Show group members
+const showGroupMembers = async () => {
+  const member_list = document.getElementById("member-list");
+  member_list.innerHTML = "";
+  const member_dropdown = document.getElementById("name-to-add");
+  member_dropdown.innerHTML =
+    "<option value='0'>-- เลือกผู้ฝากซื้อ --</option>";
+  const options = {
+    method: "GET",
+    credentials: "include",
+  };
+  await fetch(`http://${backendIPAddress}/items/members`, options)
+    .then((response) => response.json())
+    .then((data) => {
+      const members = data;
+      members.map((member) => {
+        member_list.innerHTML += `
+          <li>${member.full_name}</li>
+          `;
+        // ----------------- FILL IN YOUR CODE UNDER THIS AREA ONLY ----------------- //
+        member_dropdown.innerHTML += `<option value='${member.full_name}'>${member.full_name}</option>`;
+        // ----------------- FILL IN YOUR CODE ABOVE THIS AREA ONLY ----------------- //
+      });
+    })
+    .catch((error) => console.error(error));
+};
+
+// TODO #2.3: Send Get items ("GET") request to backend server and store the response in itemsData variable
+const getItemsFromDB = async () => {
+  const options = {
+    method: "GET",
+    credentials: "include",
+  };
+  await fetch(`http://${backendIPAddress}/items`, options)
+    .then((response) => response.json())
+    .then((data) => {
+        itemsData = data;
+    })
+    .catch((error) => console.error(error))
+  console.log(
+    "This function should fetch 'get items' route from backend server."
+  );
+};
+
+// TODO #2.4: Show items in table (Sort itemsData variable based on created_date in ascending order)
+const showItemsInTable = async (itemsData) => {
+  const table_body = document.getElementById("main-table-body");
+  table_body.innerHTML = "";
+  // ----------------- FILL IN YOUR CODE UNDER THIS AREA ONLY ----------------- //
+  itemsData = itemsData.sort((a, b) => a.created_date - b.created_date)
+  // ----------------- FILL IN YOUR CODE ABOVE THIS AREA ONLY ----------------- //
+  itemsData.map((item) => {
+    // ----------------- FILL IN YOUR CODE UNDER THIS AREA ONLY ----------------- //
+    table_body.innerHTML += `
+        <tr id="${item.item_id}">
+            <td>${item.item}</td>
+            <td>${item.name}</td>
+            <td>${item.price}</td>
+            <td><button class="delete-row" onclick="deleteItem('${item.item_id}')">ลบ</button></td>
+        </tr>
+        `;
+    // ----------------- FILL IN YOUR CODE ABOVE THIS AREA ONLY ----------------- //
+  });
+};
+
+// TODO #2.5: Send Add an item ("POST") request to backend server and update items in the table
+const addItem = async () => {
+  const item = document.getElementById("item-to-add").value;
+  const name = document.getElementById("name-to-add").value;
+  const price = document.getElementById("price-to-add").value;
+
+  const options = {
+    method: "POST",
+    credentials: "include",
+    body: JSON.stringify({
+      item,
+      name,
+      price: parseFloat(price),
+    }),
+    headers: {
+      "Content-Type": "application/json"
+    }
+  };
+  await fetch(`http://${backendIPAddress}/items`, options).catch((error) => console.error(error));
+  await getItemsFromDB();
+  showItemsInTable(itemsData);
+
+  console.log(
+    "This function should fetch 'add item' route from backend server and update items in the table."
+  );
+};
+
+// TODO 2.6: Send Delete an item ("DELETE") request to backend server and update items in the table
+const deleteItem = async (item_id) => {
+  const options = {
+    method: "DELETE",
+    credentials: "include",
+  };
+  await fetch(`http://${backendIPAddress}/items/${item_id}`, options).catch((error) => console.error(error));
+  await getItemsFromDB();
+  showItemsInTable(itemsData);
+
+  console.log(
+    "This function should fetch 'delete item' route in backend server and update items in the table."
+  );
+};
+
+const redrawDOM = () => {
+  window.document.dispatchEvent(
+    new Event("DOMContentLoaded", {
+      bubbles: true,
+      cancelable: true,
+    })
+  );
+  document.getElementById("item-to-add").value = "";
+  document.getElementById("name-to-add").value = "0";
+  document.getElementById("price-to-add").value = "";
+};
+
+document.getElementById("group-no").innerHTML = getGroupNumber();
+
+document.addEventListener("DOMContentLoaded", async function (event) {
+  console.log("Showing group members.");
+  await showGroupMembers();
+  console.log("Showing items from database.");
+  await getItemsFromDB();
+  showItemsInTable(itemsData);
+});
