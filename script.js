77d6f5ceaedba9b2c2318f25eb820d0499df98f6
PwFaze <feze2013@hotmail.com>
2023-04-27 20:20:39 +0700

    front-end

---
+++ script.js
@@ -0,0 +1,429 @@
+var schedule = {
+    "status": 1,
+    "data": [
+      {
+        "itemid": 72892,
+        "type": "schedule_item",
+        "status": 1,
+        "title": "Activity # 0 : Course Orientation",
+        "description": "Course Orientation",
+        "comment": "",
+        "datetime": "2023-01-18",
+        "weekno": "1",
+        "start_epoch": "1674009000",
+        "end_epoch": "1674019800"
+      },
+      {
+        "itemid": 72893,
+        "type": "schedule_item",
+        "status": 1,
+        "title": "Activity # 1 : Data Representation",
+        "description": "Data Representation",
+        "comment": "",
+        "datetime": "2023-01-25",
+        "weekno": "2",
+        "start_epoch": "1674613800",
+        "end_epoch": "1674624600"
+      },
+      {
+        "itemid": 72894,
+        "type": "schedule_item",
+        "status": 1,
+        "title": "Activity # 2 : CPU",
+        "description": "CPU",
+        "comment": "",
+        "datetime": "2023-02-01",
+        "weekno": "3",
+        "start_epoch": "1675218600",
+        "end_epoch": "1675229400"
+      },
+      {
+        "itemid": 72895,
+        "type": "schedule_item",
+        "status": 1,
+        "title": "Activity # 3 : OS and Linux",
+        "description": "OS and Linux",
+        "comment": "",
+        "datetime": "2023-02-08",
+        "weekno": "4",
+        "start_epoch": "1675823400",
+        "end_epoch": "1675834200"
+      },
+      {
+        "itemid": 72896,
+        "type": "schedule_item",
+        "status": 1,
+        "title": "Activity # 4 : Programming Embedded Systems",
+        "description": "Programming Embedded Systems",
+        "comment": "",
+        "datetime": "2023-02-15",
+        "weekno": "5",
+        "start_epoch": "1676428200",
+        "end_epoch": "1676439000"
+      },
+      {
+        "itemid": 72897,
+        "type": "schedule_item",
+        "status": 1,
+        "title": "Activity # 5 : Sensors",
+        "description": "Sensors",
+        "comment": "",
+        "datetime": "2023-02-22",
+        "weekno": "6",
+        "start_epoch": "1677033000",
+        "end_epoch": "1677043800"
+      },
+      {
+        "itemid": 72898,
+        "type": "schedule_item",
+        "status": 1,
+        "title": "Activity # 6 : UX/UI",
+        "description": "User Experience / User Interface",
+        "comment": "",
+        "datetime": "2023-03-01",
+        "weekno": "7",
+        "start_epoch": "1677637800",
+        "end_epoch": "1677648600"
+      },
+      {
+        "itemid": 72899,
+        "type": "schedule_item",
+        "status": 1,
+        "title": "Midterm Exam",
+        "description": "9 March 2023 8:30-11:30",
+        "comment": "",
+        "datetime": "2023-03-09",
+        "weekno": "8",
+        "start_epoch": "1678325400",
+        "end_epoch": "1678336200"
+      },
+      {
+        "itemid": 72900,
+        "type": "schedule_item",
+        "status": 1,
+        "title": "Activity # 7  : Web Development I",
+        "description": "HTML, CSS, JavaScript",
+        "comment": "",
+        "datetime": "2023-03-15",
+        "weekno": "9",
+        "start_epoch": "1678847400",
+        "end_epoch": "1678858200"
+      },
+      {
+        "itemid": 72901,
+        "type": "schedule_item",
+        "status": 1,
+        "title": "Activity # 8 : Web Development II",
+        "description": "Connecting with databases",
+        "comment": "",
+        "datetime": "2023-03-22",
+        "weekno": "10",
+        "start_epoch": "1679452200",
+        "end_epoch": "1679463000"
+      },
+      {
+        "itemid": 72902,
+        "type": "schedule_item",
+        "status": 1,
+        "title": "Activity # 9 : Machine Learning",
+        "description": "Machine Learning",
+        "comment": "",
+        "datetime": "2023-03-29",
+        "weekno": "11",
+        "start_epoch": "1680057000",
+        "end_epoch": "1680067800"
+      },
+      {
+        "itemid": 72903,
+        "type": "schedule_item",
+        "status": 1,
+        "title": "Activity # 10 : Inclusive teamwork",
+        "description": "Inclusive Teamwork\n\n \"icon_announcement.png\"Activity 10 - Inclusive Teamwork",
+        "comment": "",
+        "datetime": "2023-04-05",
+        "weekno": "12",
+        "start_epoch": "1680661800",
+        "end_epoch": "1680672600"
+      },
+      {
+        "itemid": 72904,
+        "type": "schedule_item",
+        "status": 1,
+        "title": "Activity # 11 : Computer Laws",
+        "description": "Computer Laws in Thailand",
+        "comment": "",
+        "datetime": "2023-04-12",
+        "weekno": "13",
+        "start_epoch": "1681266600",
+        "end_epoch": "1681277400"
+      },
+      {
+        "itemid": 72905,
+        "type": "schedule_item",
+        "status": 1,
+        "title": "Activity # 12 : Ethics Workshop",
+        "description": "Ethics Workshop",
+        "comment": "",
+        "datetime": "2023-04-19",
+        "weekno": "14",
+        "start_epoch": "1681871400",
+        "end_epoch": "1681882200"
+      },
+      {
+        "itemid": 72906,
+        "type": "schedule_item",
+        "status": 1,
+        "title": "Open slot",
+        "description": "TBA",
+        "comment": "",
+        "datetime": "2023-04-26",
+        "weekno": "15",
+        "start_epoch": "1682476200",
+        "end_epoch": "1682487000"
+      },
+      {
+        "itemid": 72907,
+        "type": "schedule_item",
+        "status": 1,
+        "title": "Activity # 13 : Project Presentation",
+        "description": "Project Presentation ",
+        "comment": "",
+        "datetime": "2023-05-03",
+        "weekno": "16",
+        "start_epoch": "1683081000",
+        "end_epoch": "1683091800"
+      },
+      {
+        "itemid": 72908,
+        "type": "schedule_item",
+        "status": 1,
+        "title": "Final Exam",
+        "description": "12 May 2023 8:30-11:30",
+        "comment": "",
+        "datetime": "2023-05-12",
+        "weekno": "17",
+        "start_epoch": "1683855000",
+        "end_epoch": "1683865800"
+      }
+    ]
+  }
+
+
+
+var user_course ={
+    "data": {
+      "student": [
+        {
+          "cv_cid": 29667,
+          "course_no": "2110101.05",
+          "year": "2022",
+          "semester": 1,
+          "section": "0",
+          "role": "student"
+        },
+        {
+          "cv_cid": 29362,
+          "course_no": "2100111",
+          "year": "2022",
+          "semester": 1,
+          "section": "0",
+          "role": "student"
+        },
+        {
+          "cv_cid": 29665,
+          "course_no": "2110101",
+          "year": "2022",
+          "semester": 1,
+          "section": "0",
+          "role": "student"
+        },
+        {
+          "cv_cid": 29880,
+          "course_no": "2301107",
+          "year": "2022",
+          "semester": 1,
+          "section": "0",
+          "role": "student"
+        },
+        {
+          "cv_cid": 30880,
+          "course_no": "2304103",
+          "year": "2022",
+          "semester": 1,
+          "section": "0",
+          "role": "student"
+        },
+        {
+          "cv_cid": 28924,
+          "course_no": "CU.CP.101",
+          "year": "2022",
+          "semester": 1,
+          "section": "0",
+          "role": "student"
+        },
+        {
+          "cv_cid": 28925,
+          "course_no": "CU.CP.101.PART2",
+          "year": "2022",
+          "semester": 1,
+          "section": "0",
+          "role": "student"
+        },
+        {
+          "cv_cid": 30644,
+          "course_no": "2304183",
+          "year": "2022",
+          "semester": 1,
+          "section": "0",
+          "role": "student"
+        },
+        {
+          "cv_cid": 31179,
+          "course_no": "2304103.04",
+          "year": "2022",
+          "semester": 1,
+          "section": "0",
+          "role": "student"
+        },
+        {
+          "cv_cid": 31315,
+          "course_no": "ESC.PSN",
+          "year": "2022",
+          "semester": 1,
+          "section": "0",
+          "role": "student"
+        },
+        {
+          "cv_cid": 32200,
+          "course_no": "2110215",
+          "year": "2022",
+          "semester": 2,
+          "section": "0",
+          "role": "student"
+        },
+        {
+          "cv_cid": 32873,
+          "course_no": "2301108.06",
+          "year": "2022",
+          "semester": 2,
+          "section": "0",
+          "role": "student"
+        },
+        {
+          "cv_cid": 31887,
+          "course_no": "2304184",
+          "year": "2022",
+          "semester": 2,
+          "section": "0",
+          "role": "student"
+        },
+        {
+          "cv_cid": 31543,
+          "course_no": "SHECU.026",
+          "year": "2022",
+          "semester": 2,
+          "section": "0",
+          "role": "student"
+        },
+        {
+          "cv_cid": 32201,
+          "course_no": "2110221",
+          "year": "2022",
+          "semester": 2,
+          "section": "0",
+          "role": "student"
+        },
+        {
+          "cv_cid": 33808,
+          "course_no": "2302127",
+          "year": "2022",
+          "semester": 2,
+          "section": "0",
+          "role": "student"
+        },
+        {
+          "cv_cid": 33985,
+          "course_no": "2302163",
+          "year": "2022",
+          "semester": 2,
+          "section": "0",
+          "role": "student"
+        },
+        {
+          "cv_cid": 34095,
+          "course_no": "2304104",
+          "year": "2022",
+          "semester": 2,
+          "section": "0",
+          "role": "student"
+        },
+        {
+          "cv_cid": 31531,
+          "course_no": "SHECU.012",
+          "year": "2022",
+          "semester": 2,
+          "section": "0",
+          "role": "student"
+        },
+        {
+          "cv_cid": 31927,
+          "course_no": "SHECU.004.MOOC",
+          "year": "2022",
+          "semester": 2,
+          "section": "0",
+          "role": "student"
+        },
+        {
+          "cv_cid": 31574,
+          "course_no": "SHECU.003.MOOC",
+          "year": "2022",
+          "semester": 2,
+          "section": "0",
+          "role": "student"
+        },
+        {
+          "cv_cid": 34406,
+          "course_no": "ESC.PSN",
+          "year": "2022",
+          "semester": 2,
+          "section": "0",
+          "role": "student"
+        },
+        {
+          "cv_cid": 34598,
+          "course_no": "CUVIPC009",
+          "year": "2022",
+          "semester": 2,
+          "section": "0",
+          "role": "student"
+        }
+      ]
+    },
+    "status": 1
+  }
+function expandDetails() {
+  var btn = document.getElementsByClassName("content");
+  var i = 0;
+  for (i ; i < btn.length ; i++) {
+    if (btn[i].style.display === 'none') {
+      btn[i].style.display = 'flex';
+    }
+    else {
+      btn[i].style.display = 'none';
+    }
+  }
+}
+
+window.onscroll = function() {scrollFunction()};
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    document.getElementById("navbar").style.top = "0";
+  } else {
+    document.getElementById("navbar").style.top = "-50px";
+  }
+}
+
+
+
+
+
