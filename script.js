1d8a1cc31333630bb6964bfffcbf01c79c221598
PwFaze <feze2013@hotmail.com>
2023-05-02 03:13:36 +0700

    adding transition

--- script.js
+++ script.js
@@ -3,7 +3,10 @@
 const backendIPAddress = "127.0.0.1:3000";
 
 let itemsData;
-const data = new Date();
+const currentDate = new Date();
+const firstDate = new Date(2023, 0, 9);
+const lastDateOfSemester = new Date(2023, 4, 5);
+
 var numberOfCheckBox = 0;
 
 const updateProgress = async (name, progress) => {
@@ -13,7 +16,7 @@ const updateProgress = async (name, progress) => {
     method: "PUT",
     credentials: "include",
     body: JSON.stringify({
-      "prog": progress,
+      "prog": progress | 0,
       "subject": name,
       "student_id": studentId + name
     }),
@@ -161,14 +164,44 @@ function generateNewSubject(subjectName) {
   const subjectDiv = document.createElement('div');
   subjectDiv.classList.add(subjectName);
 
-  const subj = document.createElement('h2');
+  const subj = document.createElement('span');
   subj.innerHTML = subjectName;
+  subj.style.padding = '10px';
+  subj.style.color = "#333";
+  subj.style.fontSize = "2em";
+  subj.style.fontFamily = "'Bebas Neue', cursive";
+  subj.style.fontWeight = "500";
+  subj.style.position = "flex";
+  subj.style.borderRadius = "10px";
+  subj.style.top = "50%";
+  subj.style.left = "50%";
+  subj.style.transform = "translate(-50%,-50%)";
+  subj.style.backgroundImage = "linear-gradient(gold, gold)";
+  subj.style.backgroundSize = "100% 10px";
+  subj.style.backgroundRepeat = "no-repeat";
+  subj.style.backgroundPosition = "100% 0%";
+  subj.style.transition = "background-size .7s, background-position .5s ease-in-out";
+
+// Add hover style to the new div
+  subj.addEventListener("mouseover", function() {
+  subj.style.backgroundSize = "100% 100%";
+  subj.style.backgroundPosition = "0% 100%";
+  subj.style.transition = "background-position .7s, background-size .5s ease-in-out";
+
+  
+});
+
   subjectDiv.appendChild(subj);
 
   return subjectDiv
 }
 
-function generateTeacherBar(teacherProgress) {
+function generateTeacherBar() {
+
+  const diffTimeFirstLast = Math.ceil(Math.abs(lastDateOfSemester - firstDate) / (1000 * 60 * 60 * 24));
+  const diffTimeFirstCurrent = Math.ceil(Math.abs(firstDate - currentDate) / (1000 * 60 * 60 * 24));
+    
+  const teacherProgress = parseInt(diffTimeFirstCurrent / diffTimeFirstLast * 100);
 
   const teacher = document.createElement("div");
   teacher.classList.add("teacher");
@@ -176,6 +209,7 @@ function generateTeacherBar(teacherProgress) {
   const teacherBarName = document.createElement('h2');
   teacherBarName.innerHTML = "teacher-progress"
   teacherBarName.classList.add("barName");
+  teacherBarName.style.fontFamily = "monospace";
   teacher.appendChild(teacherBarName);
 
   const progressBar = document.createElement('div');
@@ -203,7 +237,9 @@ function generateTeacherBar(teacherProgress) {
   const style = document.createElement('style');
   document.head.appendChild(style);
   style.sheet.insertRule(keyframes, 0);
-  teacherProgressBar.style.animation = 'teacherMoving 1s linear forwards';
+  // teacherProgressBar.style.animation = 'teacherMoving 1s linear forwards';
+  teacherProgressBar.style.width = teacherProgress + "%";
+  teacherProgressBar.style.transition = "width 1s linear";
 
   // adding animation finished
 
@@ -215,16 +251,15 @@ function generateTeacherBar(teacherProgress) {
   return teacher;
 }
 
-function generateStudentBar(studentProgress) {
-
-  studentProgress = parseInt(studentProgress, 10);
+function generateStudentBar(subjectName, studentProgress) {
 
   const student = document.createElement("div");
   student.classList.add("student");
 
 
   const studentBarName = document.createElement('h2');
-  studentBarName.innerHTML = "student-progress"
+  studentBarName.innerHTML = "student-progress";
+  studentBarName.style.fontFamily = "monospace";
   studentBarName.classList.add("barName");
   student.appendChild(studentBarName);
 
@@ -233,7 +268,7 @@ function generateStudentBar(studentProgress) {
   student.appendChild(progressBar);
 
   const studentProgressBar = document.createElement("div");
-  studentProgressBar.classList.add('studentProgressBar');
+  studentProgressBar.classList.add('studentProgressBar_' + subjectName);
   progressBar.appendChild(studentProgressBar);
   studentProgressBar.style.width = "0%";
   studentProgressBar.style.background = "#e91e63";
@@ -241,19 +276,26 @@ function generateStudentBar(studentProgress) {
   studentProgressBar.style.borderRadius = "5px";
 
   // adding animantion
-
+  
   const keyframes = `
-      @keyframes studentMoving {
-          100% {
-              width: ${studentProgress + "%"};
-          }
-      }
+    @keyframes studentMoving_${subjectName} {
+        100% {
+            width: ${studentProgress + "%"};
+        }
+    }
   `;
-
+  
   const style = document.createElement('style');
   document.head.appendChild(style);
   style.sheet.insertRule(keyframes, 0);
-  studentProgressBar.style.animation = 'studentMoving 1s linear forwards';
+  // studentProgressBar.style.animation = `studentMoving_${subjectName} 1s linear forwards`;
+  studentProgressBar.style.width = studentProgress + "%";
+  studentProgressBar.style.transition = "width 1s linear";
+
+  setTimeout(() => {
+    studentProgressBar.style.width = studentProgress + "%";
+  }, 1000);
+  
 
   // adding animation finished
 
@@ -261,9 +303,56 @@ function generateStudentBar(studentProgress) {
   studentProgressBox.classList.add("progressBox");
   studentProgressBox.innerHTML = studentProgress;
   studentProgressBar.appendChild(studentProgressBox);
-
+  
   return student;
 }
+
+  // studentProgress = parseInt(studentProgress, 10);
+
+  // const student = document.createElement("div");
+  // student.classList.add("student");
+
+
+  // const studentBarName = document.createElement('h2');
+  // studentBarName.innerHTML = "student-progress"
+  // studentBarName.classList.add("barName");
+  // student.appendChild(studentBarName);
+
+  // const progressBar = document.createElement('div');
+  // progressBar.classList.add("progressBar");
+  // student.appendChild(progressBar);
+
+  // const studentProgressBar = document.createElement("div");
+  // studentProgressBar.classList.add('studentProgressBar');
+  // progressBar.appendChild(studentProgressBar);
+  // studentProgressBar.style.width = "0%";
+  // studentProgressBar.style.background = "#e91e63";
+  // studentProgressBar.style.height = "10px";
+  // studentProgressBar.style.borderRadius = "5px";
+
+  // // adding animantion
+
+  // const keyframes = `
+  //     @keyframes studentMoving {
+  //         100% {
+  //             width: ${studentProgress + "%"};
+  //         }
+  //     }
+  // `;
+
+  // const style = document.createElement('style');
+  // document.head.appendChild(style);
+  // style.sheet.insertRule(keyframes, 0);
+  // studentProgressBar.style.animation = 'studentMoving 1s linear forwards';
+
+  // // adding animation finished
+
+  // const studentProgressBox = document.createElement("span");
+  // studentProgressBox.classList.add("progressBox");
+  // studentProgressBox.innerHTML = studentProgress;
+  // studentProgressBar.appendChild(studentProgressBox);
+
+  // return student;
 
 
 // function makeCheckBox(data) {
@@ -299,7 +388,7 @@ function generateStudentBar(studentProgress) {
 // <!-- !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!edit by Rose, if(error): uncomment the code above !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 
 function makeCheckBox(data) {
-  const details = document.createElement('div');
+  const details = document.createElement('section');
   details.classList.add('progressDetails');
   details.style.display = 'none';
 
@@ -388,7 +477,7 @@ function createDetailsButton(scheduleData) {
 
 function createSubmitButton(name) {
   const submitContainer = document.createElement('div');
-
+  let progress = 0;
   const submitButton = document.createElement('button');
   submitButton.classList.add('Submit');
   submitButton.id = name;
@@ -405,8 +494,18 @@ function createSubmitButton(name) {
       }
     }
     console.log(c / checkboxes.length * 100);
-    updateProgress(name, c / checkboxes.length * 100);
+    let progress = c / checkboxes.length * 100;
+    progress = parseInt(progress);
+    updateProgress(name, progress);
+
+    const studentProgressBar = document.querySelector('.studentProgressBar_' + name);
+    studentProgressBar.style.width = progress + "%";
+    studentProgressBar.style.transition = "width 1s linear";
+
+    const studentProgressBox = studentProgressBar.children[0];
+    studentProgressBox.innerHTML = progress;
   };
+
 
   submitContainer.appendChild(submitButton);
 
@@ -424,7 +523,7 @@ function expandDetails(detailsDiv) {
 
 // function combineSubject(name, teacherProgress, studentProgress, scheduleData) {
 //   const subj = generateNewSubject(name);
-//   const subjTeacher = generateTeacherBar(teacherProgress);
+//   const subjTeacher = generateTeacherBar();
 //   const subjStudent = generateStudentBar(studentProgress);
 //   const subjDetails = createDetailsButton(scheduleData);
 //   const subjSubmitButton = createSubmitButton();
@@ -437,10 +536,10 @@ function expandDetails(detailsDiv) {
 //   document.querySelector('.subjects').appendChild(subj);
 // }
 
-function combineSubject(name, teacherProgress, studentProgress, scheduleData) {
+function combineSubject(name, studentProgress, scheduleData) {
   const subj = generateNewSubject(name);
-  const subjTeacher = generateTeacherBar(teacherProgress);
-  const subjStudent = generateStudentBar(studentProgress);
+  const subjTeacher = generateTeacherBar();
+  const subjStudent = generateStudentBar(name, studentProgress);
   const subjDetails = createDetailsButton(scheduleData);
   const subjSubmitButton = createSubmitButton(name);
 
@@ -474,19 +573,34 @@ function createBarFromCV_CID(userCourses) {
     const scheduleData = await getCourseSchedule(cid);
     const materialsData = await getCourseMaterials(cid);
     const courseName = await getCourseInfo(cid);
-    const subjectName = courseName["data"]["title"].replace(/ /g, "_");
+    const subjectName = courseName["data"]["title"].replace(/[()]/g, '');
+    let newSubjectName = "";
+    for (const char of subjectName) {
+      if (char == " ") {
+        newSubjectName += "_";
+      }
+      else if (char == "[" || char == "]") {
+        continue;
+      }
+      else {
+        newSubjectName += char;
+      }
+
+    }
+
+
     const data = scheduleData["data"].length < 10 ? materialsData : scheduleData;
-    return { subjectName, data };
+    return { newSubjectName, data };
   });
 
   Promise.all(promises).then(async (results) => {
-    results.sort((a, b) => a.subjectName.localeCompare(b.subjectName));
+    results.sort((a, b) => a.newSubjectName.localeCompare(b.newSubjectName));
     for (const result of results) {
       const studentId = await getUserProfile();
-      console.log(studentId + result.subjectName);
-      const studentProgress = await getSubjectProgress(studentId + result.subjectName);
+      console.log(studentId + result.newSubjectName);
+      const studentProgress = await getSubjectProgress(studentId + result.newSubjectName);
       console.log(studentProgress);
-      combineSubject(result.subjectName, "80", studentProgress, result.data);
+      combineSubject(result.newSubjectName, studentProgress, result.data);
     }
   });
 }
@@ -519,20 +633,6 @@ function createBarFromCV_CID(userCourses) {
 //   createBarFromCV_CID(userCourses);
 // });
 
-// async function getItemsFromDB() {
-//   try {
-//     const options = {
-//       method: "GET",
-//       credentials: "include",
-//     };
-//     const response = await fetch(`http://${backendIPAddress}/items`, options);
-//     const data = await response.json();
-//     itemsData = data;
-//     console.log("This function should fetch 'get items' route from backend server.");
-//   } catch (error) {
-//     console.error(error);
-//   }
-// }
 
 
 // createAllBar(userCourses, scheduleData);
@@ -541,6 +641,3 @@ function createBarFromCV_CID(userCourses) {
   createBarFromCV_CID(userCourses);
 })();
 
-// changeStudentBar("General_Chemistry__[Section_1-2]", 70);
-let a = document.getElementsByClassName("General_Chemistry__[Section_1-2]");
-console.log(a);
